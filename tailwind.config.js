59d93b15e45170a4b58f4b91fa3750b2635bcd2b
SSD <sibghatkarimi777@gmail.com>
2023-06-14 19:02:49 +0500

    first

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  fontFamily:{
+    Mulish: ['Mulish'],
+  },
+  plugins: [],
+
+  colors: {
+    transparent: 'transparent',
+    current: 'currentColor',
+    'white': '#ffffff',
+    'purple': '#3f3cbb',
+    'midnight': '#121063',
+    'metal': '#565584',
+    'tahiti': '#3ab7bf',
+    'silver': '#ecebff',
+    'bubble-gum': '#ff77e9',
+    'bermuda': '#78dcca',
+    'black': '#18181B',
+    'black':'#27272A',
+// 'gradient':'#06B6D4',
+// 'gradient':'#0E7490',
+// 'gradient':'#0284C7',
+
+  },
+}
+
