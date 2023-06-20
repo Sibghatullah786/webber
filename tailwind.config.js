c369fd4a0953d621a58f42fb599a1ca655b71638
SSD <sibghatkarimi777@gmail.com>
2023-06-20 20:54:20 +0500

    This is my first Task I have created with Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,36 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
-  },
-  fontFamily:{
-    Mulish: ['Mulish'],
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'black': '#18181B',
+        'black':'#27272A',
+        'gradient':'#06B6D4',
+        'gradient':'#0E7490',
+        'gradient':'#0284C7',
+        
+      },
+      backgroundImage:{
+        'section-gradient': 'linear-gradient(274.03deg, #06B6D4 0%, #0E7490 0.01%, #0284C7 100%)'
+      },
+
+      fontFamily:{
+        Mulish: ['Mulish'],
+        nunito: ["'Nunito Sans', sans-serif"],
+        montserrat:["'Montserrat', sans-serif"]
+      },
+    },
   },
   plugins: [],
-
-  colors: {
-    transparent: 'transparent',
-    current: 'currentColor',
-    'white': '#ffffff',
-    'purple': '#3f3cbb',
-    'midnight': '#121063',
-    'metal': '#565584',
-    'tahiti': '#3ab7bf',
-    'silver': '#ecebff',
-    'bubble-gum': '#ff77e9',
-    'bermuda': '#78dcca',
-    'black': '#18181B',
-    'black':'#27272A',
-// 'gradient':'#06B6D4',
-// 'gradient':'#0E7490',
-// 'gradient':'#0284C7',
-
-  },
 }
 
